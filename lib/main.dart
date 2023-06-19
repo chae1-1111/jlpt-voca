1a9ecf315eee4929b6bfe844a5af229aaeb1a690
chae1-1111 <dla4212@naver.com>
2023-06-19 20:03:32 +0900

    feat: ThemeData 추가
    ThemeData 추가
    - Appbar style 적용

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "JLPT 단어장",
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          titleTextStyle: const TextStyle(
+            fontSize: 25,
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+          backgroundColor: Colors.yellow[100],
+          foregroundColor: Colors.black,
+          elevation: 0,
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("JLPT 단어장"),
+          title: const Text(
+            "JLPT 단어장",
+          ),
         ),
         body: Container(),
       ),
